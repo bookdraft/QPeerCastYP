4e7b0f9c1ff5ff1cd404ceebdbf9b68e76b7a687
ciao <ciao@users.sourceforge.jp>
2008-09-09 04:16:47 +0900

    build.bat: qtvars.bat を使うようにした

--- build.bat
+++ build.bat
@@ -1,8 +1,3 @@
-set QTDIR=C:\Qt\4.4.0
-set PATH=C:\Qt\4.4.0\bin
-set PATH=%PATH%;C:\MinGW\bin
-set PATH=%PATH%;%SystemRoot%\System32
-set QMAKESPEC=win32-g++
-
+call C:\Qt\4.4.0\bin\qtvars.bat
 qmake
 make && release\qpeercastyp.exe || pause
